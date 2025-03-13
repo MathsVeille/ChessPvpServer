03ad0dc2abc7c35ef1bafaa8e27c4383ccf94639
MathsVeille <matvey.sienichev@gmail.com>
2025-03-13 23:10:19 +0100

    fonctionnel

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { Server, Socket } from "socket.io";
 import {performance}  from 'perf_hooks'; 
 //import {Player} from'./Player.js';
-type Player = {piece:number, x:number, z:number, x_change:number, z_change:number, speed:number}
+type Player = {piece:number, x:number, z:number, x_change:number, z_change:number, speed:number, direction:string} //dir:"+x"/"-x"/"+z"/"-z"
 
 
 
@@ -36,25 +36,49 @@ io.on("connection", (socket)=>{
         console.log("emmited:", key, value.x, value.z);
         
     });
-
-    //players.set(socket.id, {piece: 1, x:1, z:1, x_change:0, z_change:0});
-
-    //socket.broadcast.emit("new_player", {id:socket.id, x:1, z:1});
     
     console.log("new player " + socket.id);
     console.log(players);
 
     socket.on("desired_player_pos", desiredPos=>{
-        //let old_xchange = players.get(socket.id)?.x_change;
-        //let old_zchange = players.get(socket.id)?.z_change;
 
         //en gros, on rajoute le delta position demandé par le client au delata P deja existant.
         let player = players.get(socket.id);
         if(!player)throw Error("no player in function desired player pos");
         player.x_change +=  desiredPos.x;
         player.z_change +=  desiredPos.z;
-        
+        //giga function
+        if(desiredPos.x>0){
+            if(player.direction == "+x")player.speed += 0.4;
+            else{
+                player.speed = 1; 
+                player.direction = "+x";
+            }
+        }
+        if(desiredPos.x<0){
+            if(player.direction == "-x")player.speed += 0.4;
+            else{
+                player.speed = 1; 
+                player.direction = "-x";
+            }
+        }
+        if(desiredPos.z>0){
+            if(player.direction == "+z")player.speed += 0.4;
+            else{
+                player.speed = 1; 
+                player.direction = "+z";
+            }
+        }
+        if(desiredPos.z<0){
+            if(player.direction == "-z")player.speed += 0.4;
+            else{
+                player.speed = 1; 
+                player.direction = "-z";
+            }
+        }
 
+
+        //fin giga function
         players.set(socket.id, player);
         //
 
@@ -63,7 +87,7 @@ io.on("connection", (socket)=>{
     });
 
     socket.on("player_init", (x, z)=>{
-        players.set(socket.id, {piece:1, x:x, z:z, x_change:0, z_change:0, speed:0});
+        players.set(socket.id, {piece:1, x:x, z:z, x_change:0, z_change:0, speed:0, direction:""});
         socket.broadcast.emit("new_player", {id:socket.id, x:x, z:z});
     });
 
@@ -93,70 +117,114 @@ function gameLoop(){
 
     
     
-    players.forEach((value, key1)=>{
+    players.forEach((player1, key1)=>{
 
         //on delete les players qui se touchent
-        players.forEach((player, key2)=>{if(value != player &&Math.round(player.x) == Math.round(value.x) && Math.round(player.z) == Math.round(value.z)){
+        players.forEach((player2, key2)=>{if(player1 != player2 &&Math.round(player2.x) == Math.round(player1.x) && Math.round(player2.z) == Math.round(player1.z)){
+            let looser1 = null;
+            let looser2 = null;
+            if(player1.speed > player2.speed){
+                looser1 = key2;
+            }else if(player2.speed > player1.speed){
+                looser1 = key1
+            }else{//meme vitesse, autodestruction
+                looser1 = key1;
+                looser2 = key2;
+            }
+
             sockets.forEach((sock)=>{
-                if (sock.id == key1 || sock.id == key2){
+                if (sock.id === looser1 || sock.id === looser2){
                     sock.emit("u_dead");
                 }else{
-                    sock.emit("player_left", key1);
-                    sock.emit("player_left", key2);
+                    sock.emit("player_left", looser1);
+                    if(looser2 != null){
+                        sock.emit("player_left", looser2);
+                    }   
                 }
             });
-            players.delete(key1);
-            players.delete(key2);
+            players.delete(looser1);
+            if(looser2 !== null){ //will not delete if looser2 == null
+                players.delete(looser2);
+            }
+             
         }})
 
-        //on met à jour le tableau interne des positions des joeurs
-        if(value.x_change != 0){
-            console.log(value.x);
-            console.log("change "+value.x_change);
-            if(value.x_change > 0){
-                if((value.x_change - delta)<0){  //si la valeur de changementy est proche de 0 on arrondit la position et on la met à 0, evite les osilements 
-                    value.x = Math.round(value.x); 
-                    value.x_change = 0; 
+        //MAJ X player1S
+        if(player1.x_change != 0){
+            if(player1.x_change > 0){
+                if((player1.x_change - delta*player1.speed)<0){  //si la valeur de changementy est proche de 0 on arrondit la position et on la met à 0, evite les osilements 
+                    player1.x = Math.round(player1.x); 
+                    player1.x_change = 0; 
+                    player1.speed = 1;
                 }
                 else{
-                    value.x += (delta);
-                    value.x_change -= (delta);
+                    player1.x += (delta*player1.speed);
+                    player1.x_change -= (delta*player1.speed);
                 } 
                 
-            }else if(value.x_change <0){
-                if((value.x_change + delta)>0){  //idem ici, on arrondit pour éviter les oscilations
-                    value.x = Math.round(value.x); 
-                    value.x_change = 0; 
+            }else if(player1.x_change <0){
+                if((player1.x_change + delta*player1.speed)>0){  //idem ici, on arrondit pour éviter les oscilations
+                    player1.x = Math.round(player1.x); 
+                    player1.x_change = 0; 
+                    player1.speed = 1;
                 }
                 else{
-                    value.x -= (delta);
-                    value.x_change += (delta);
+                    player1.x -= (delta*player1.speed);
+                    player1.x_change += (delta*player1.speed);
                 }
                 
             }
             
         }
+        //MAJ Z player1S
+        if(player1.z_change != 0){
+            if(player1.z_change > 0){
+                if((player1.z_change - delta*player1.speed)<0){  //si la valeur de changementy est proche de 0 on arrondit la position et on la met à 0, evite les osilements 
+                    player1.z = Math.round(player1.z); 
+                    player1.z_change = 0; 
+                    player1.speed = 1;
+                }
+                else{
+                    player1.z += (delta*player1.speed);
+                    player1.z_change -= (delta*player1.speed);
+                } 
+                
+            }else if(player1.z_change <0){
+                if((player1.z_change + delta*player1.speed)>0){  //idem ici, on arrondit pour éviter les oscilations
+                    player1.z = Math.round(player1.z); 
+                    player1.z_change = 0; 
+                    player1.speed = 1;
+                }
+                else{
+                    player1.z -= (delta*player1.speed);
+                    player1.z_change += (delta*player1.speed);
+                }
+                
+            }
+            
+        }
+
     });
 
     //on syncronise tout toutes les 2s 
     if(accumulator > 2){
-        let syncArray :{id:string, x:number, z:number, x_change:number, z_change:number}[] = [];
+        let syncArray :{id:string, x:number, z:number, x_change:number, z_change:number, speed:number, direction:String}[] = [];
         players.forEach((value, key)=>{
-            syncArray.push({id:key, x:value.x, z:value.z, x_change:value.x_change, z_change:value.z_change});
+            syncArray.push({id:key, x:value.x, z:value.z, x_change:value.x_change, z_change:value.z_change, speed:value.speed, direction:value.direction});
         })
 
         
         sockets.forEach((sock) => {
             sock.emit("sync", syncArray);
         });
-        accumulator -= 10;
+        accumulator -= 1;
         console.log("Synchro");
         console.log(syncArray);
     }
     accumulator += delta;
     //////////////////////////////////////////////////////////
 
-    //on relance la fonction 
+    //on relance la fonction pepere 10ms de repos pour le serv
     setTimeout(gameLoop, 10);
     //setImmediate(gameLoop) //plus rapide mais pas envie de surchgarger le serveur pour l'instant
 }
